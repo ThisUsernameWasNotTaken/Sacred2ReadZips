b9afb685acb58c9e840c51b6991695d21d2d711a
Reiner Zufall <ruben.boeck@googlemail.com>
2023-09-24 21:59:57 +0200

    modules idk

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![allow(non_snake_case)]
 
+mod sacredFolderFunctions;
+
 use std::{fs, io};
 use std::error::Error;
 use std::io::stdin;
 use zip;
+use sacredFolderFunctions::sacred::*;
 
 fn main() {
     loop {
@@ -15,57 +18,10 @@ fn main() {
             continue;
         }
 
-        let resReadZip = read_all_filepaths_from_zip(&input_path).unwrap();
+        let resReadZip = readZip(&input_path).unwrap();
         //println!("{resReadZip:?}");
-        let files = resReadZip.iter().map(|x| { x.clone().zipInsidePath.clone() });
-        let x: Vec<String> = files.collect();
-        let y = x.join("\r\n");
-        println!("{}", y);
+        let files = listAllInsidePaths(resReadZip).join("\r\n");
+        println!("{}", files);
     }
 }
 
-// #[derive(Debug)]
-// enum TryFileError {
-//     UnknownError(Box<str>)
-//     // ErrorStdReadLine,
-//     // ErrorOpenFile,
-// }
-
-fn read_all_filepaths_from_zip(input_path: &String) -> Result<Vec<SacredZipFile>, Box<dyn Error>> {
-    let filepath = std::path::PathBuf::from(&*input_path.trim());
-    let file_res = std::fs::File::open(filepath.clone());
-    if file_res.is_err() {
-        println!("{filepath:?}")
-    }
-    let file = file_res?;
-
-    let mut sacredFiles: Vec<SacredZipFile> = Vec::new();
-    let mut archive = zip::ZipArchive::new(file).unwrap();
-    for i in 0..archive.len() {
-        let SacredZipFile { zipPath, zipInsidePath, endsWithSlash, name, comment }: SacredZipFile;
-
-        let mut archiveZipFile = archive.by_index(i).unwrap();
-        let archiveZipFilePath = match archiveZipFile.enclosed_name() {
-            Some(path) => path.to_owned(),
-            None => continue,
-        };
-
-        zipPath = filepath.clone().into_os_string().into_string().unwrap();
-        zipInsidePath = archiveZipFilePath.into_os_string().into_string().unwrap();
-        endsWithSlash = (*archiveZipFile.name()).ends_with('/');
-        name = archiveZipFile.name().to_string();
-        comment = archiveZipFile.comment().to_string();
-
-        sacredFiles.push(SacredZipFile { zipPath, zipInsidePath, endsWithSlash, name, comment });
-    }
-    return Ok(sacredFiles);
-}
-
-#[derive(Debug)]
-struct SacredZipFile {
-    zipPath: String,
-    zipInsidePath: String,
-    endsWithSlash: bool,
-    name: String,
-    comment: String,
-}
---
+++ src/sacredFolderFunctions.rs
@@ -0,0 +1,49 @@
+#![allow(non_snake_case)]
+
+use zip;
+
+pub mod sacred {
+    pub fn listAllInsidePaths(sacredFiles: Vec<SacredZipFile>) -> Vec<String>
+    {
+        return sacredFiles.iter().map(|x| { x.clone().zipInsidePath.clone() }).collect::<Vec<String>>();
+    }
+
+    pub fn readZip(filepathSacredZip: &String) -> Result<Vec<SacredZipFile>, Box<dyn std::error::Error>> {
+        let filepath = std::path::PathBuf::from(&*filepathSacredZip.trim());
+        let file_res = std::fs::File::open(filepath.clone());
+        if file_res.is_err() {
+            println!("{filepath:?}")
+        }
+        let file = file_res?;
+
+        let mut sacredFiles: Vec<SacredZipFile> = Vec::new();
+        let mut archive = zip::ZipArchive::new(file).unwrap();
+        for i in 0..archive.len() {
+            let SacredZipFile { zipPath, zipInsidePath, endsWithSlash, name, comment }: SacredZipFile;
+
+            let mut archiveZipFile = archive.by_index(i).unwrap();
+            let archiveZipFilePath = match archiveZipFile.enclosed_name() {
+                Some(path) => path.to_owned(),
+                None => continue,
+            };
+
+            zipPath = filepath.clone().into_os_string().into_string().unwrap();
+            zipInsidePath = archiveZipFilePath.into_os_string().into_string().unwrap();
+            endsWithSlash = (*archiveZipFile.name()).ends_with('/');
+            name = archiveZipFile.name().to_string();
+            comment = archiveZipFile.comment().to_string();
+
+            sacredFiles.push(SacredZipFile { zipPath, zipInsidePath, endsWithSlash, name, comment });
+        }
+        return Ok(sacredFiles);
+    }
+
+    #[derive(Debug)]
+    pub struct SacredZipFile {
+        zipPath: String,
+        zipInsidePath: String,
+        endsWithSlash: bool,
+        name: String,
+        comment: String,
+    }
+}
